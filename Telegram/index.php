f14e0f2daf4a44eb6b024bfc9e1b9f7df09838c0
yurimaradini <6190137@itisrossi.vi.it>
2023-01-26 20:30:39 +0100

    ci sono quasi
    troppa roba

--- Telegram/index.php
+++ Telegram/index.php
@@ -29,6 +29,8 @@ while(true){
 		$chatId=$message->getChat()->getId();
 		$text=$message->getText();
 
+		$competitionId = null;
+
 		if($text == "/start") {
 			$response = $client->sendMessage([
 				'chat_id' => $chatId,
@@ -42,55 +44,150 @@ while(true){
 			$risultato = "Risultato: 5⃣-5⃣";
 			$marcatori = "🔵🔵 14' VICTOR OSIMHEN\n🔵🔵 39' KVARACOSO\n⚫⚪ 42' ANGÈL DI MARIA\n🟨  44' DANILO\n🔵🔵 55' AMIR RRAHMANI\n🔵🔵 65' VICTOR OSIMHEN\n🔵🔵 72' ELJIF ELMAS\n⚫⚪ 90' DUSANSESSUALE\n⚫⚪ 90+1' DUSANSESSUALE\n⚫⚪ 90+2' PLUSVALENZA\n⚫⚪ 90+3' PLUSVALENZA\n";
 
-			send($chatId, $giornoPartita, $squadre, $risultato, $marcatori);
+			sendMatch($chatId, $giornoPartita, $squadre, $risultato, $marcatori);
 		}
+		// else if ($text == "/italia") {
+
+		// 	$competitionId = 47;
+		// 	//$url = 'https://livescore-api.com/api-client/countries/list.json?date=today&key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V';
+		// 	$url = 'https://livescore-api.com/api-client/competitions/list.json?key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V&country_id='. $competitionId;
+		// 	$res = json_decode(file_get_contents($url), true);
+		// 	$competitions = $res['data']['competition'];
+
+		// 	$leauges = "";
+		// 	foreach ($competitions as $c) {
+		// 		$leauges .= $c . "\n";
+		// 	}
+
+		// 	sendLeagues($chatId, $leauges);
+		// }
 		else {
-			$res = json_decode(file_get_contents('https://livescore-api.com/api-client/scores/events.json?id=129180&key=KApavnX28UfvETOH&secret=yiACMJpHfQlSG5YrFoBaLycTtsp8mdPU'), true);
-			$data = $res['data'];
-			$match = $data['match'];
+			$countriesId = [47];//, 19, 43, 21, 1];
+			$countryIndex = 0;
 
-			$giornoPartita = "📆Giorno: " . $match['date'];
-			$squadre = "Partita: " . $match['home_name'] . " - " . $match['home_name'];
-			$risultato = "Risultato: " . $match['score'];
+			$countriesUrl = "https://livescore-api.com/api-client/countries/list.json?key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V";
+			$teamsUrl = "https://livescore-api.com/api-client/teams/list.json?key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V";
+			$teamId = null;
+			
+			$page = 1;
+			//			$res = json_decode(file_get_contents('https://livescore-api.com/api-client/teams/list.json?key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V&country_id=47'), true);
+			
+			while ($teamId == null) {
+				
+				//Tutti i paesi sono già stati controllati
+				if($countryIndex == count($countriesId)) {
+					sendMatch($chatId, "Nessuna squadra trovata.", "", "", "");
+					break;
+				}
+				
+				//ricerca lista di squadre per country e per pagina
+				$res = json_decode(file_get_contents($teamsUrl . "&country_id=" . $countriesId[$countryIndex] . "&page=" . $page), true);
+				$data = $res['data'];
+				$teams = $data['teams'];
 
-			$marcatori = "";
-			if($risultato != "0-0") {
-				$marcatori = "Marcatori:\n";
-				foreach ($data['event'] as $event) {
-					if ($event['event'] == "GOAL") {
-						$marcatori .= $event['time'] ."'". "  ⚽ - " . $event['player'] . "\n";
-					}
-					else if ($event['event'] == "OWN_GOAL") {
-						$marcatori .= $event['time'] ."'". "  ↩️ - " . $event['player'] . "\n";
-					}
-					else if ($event['event'] == "GOAL_PENALTY") {
-						$marcatori .= $event['time'] ."'". " ®🥅 - " . $event['player'] . "\n";
-					}
-					else if ($event['event'] == "YELLOW_CARD") {
-						$marcatori .= $event['time'] ."'". "  🟨 - " . $event['player'] . "\n";
-					}
-					else if ($event['event'] == "RED_CARD" || $event['event'] == "YELLOW_RED_CARD") {
-						$marcatori .= $event['time'] ."'". "  🟥 - " . $event['player'] . "\n";
-					}
-					else if ($event['event'] == "MISSED_PENALTY") {
-						$marcatori .= $event['time'] ."'". " ®❌ - " . $event['player'] . "\n";
+				foreach ($teams as $team) {
+					//controllo se il nome è uguale, se lo trovo salvo l'id e chiamo findMatch
+					if (strtoupper($team['name']) == strtoupper($text)) {
+						$teamId = $team['id'];
+						findMatch($teamId);
+						break;
 					}
 				}
+				
+				//se ho finito di controllare le squadre in questa pagina, passo alla prossima pagina
+				if ($data['next_page']) {
+					$page++;
+				}
+				//se non ci sono altre pagine, passo al prossimo paese
+				else {
+					$countryIndex++;
+				}
 			}
-
-			send($chatId, $giornoPartita, $squadre, $risultato, $marcatori);
-
 		}
-		//cd ../../xampp/htdocs/telegram
 	}
 }
 
-function send($chatId, $giornoPartita, $squadre, $risultato, $marcatori) {
+function findMatch($teamId) {
+	$matchUrl = "http://livescore-api.com/api-client/scores/history.json?key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V";
+
+	$page = 1;
+
+	$res = json_decode(file_get_contents($matchUrl . "&team=" . $teamId . "&page=" . $page), true);
+	$data = $res['data'];
+	$matches = $data['matche'];	
+
+	$page = $data['total_pages'];
+
+	$completeMessage = "";
+	for($i = 0; $i < 5; $i++) {
+		$completeMessage .= writeMatch($matches[count($matches) - 1 - $i]);
+	}
+	
+
+			
+
+	// 		sendMatch($chatId, $giornoPartita, $squadre, $risultato, $marcatori);
+
+}
+
+function writeMatch($match) {
+	$giornoPartita = "📆Giorno: " . $match['date'];
+	$squadre = "Partita: " . $match['home_name'] . " - " . $match['home_name'];
+	$risultato = "Risultato: " . $match['score'];
+
+	$marcatori = "";
+	if($risultato != "0-0") {
+		$marcatori .= getEvents($match['id']);
+	}
+
+	return $giornoPartita . "\n" . $squadre . "\n" . $risultato . "\n" . $marcatori . "\t---   ---   ---   ---";
+}
+
+function getEvents($matchId) {
+	$matchUrl = "https://livescore-api.com/api-client/scores/events.json?key=EC1cUn35m2S80WdY&secret=MJZysWmZywk6cHxQky3fW7ki9cra8E8V";
+	$res = json_decode(file_get_contents($matchUrl . "&id=" . $matchId), true);
+			$data = $res['data'];
+			//$match = $data['match'];
+
+	$marcatori = "Marcatori:\n";
+		foreach ($data['event'] as $event) {
+			if ($event['event'] == "GOAL") {
+				$marcatori .= $event['time'] ."'". "  ⚽ - " . $event['player'] . "\n";
+			}
+			else if ($event['event'] == "OWN_GOAL") {
+				$marcatori .= $event['time'] ."'". "  ↩️ - " . $event['player'] . "\n";
+			}
+			else if ($event['event'] == "GOAL_PENALTY") {
+				$marcatori .= $event['time'] ."'". " ®🥅 - " . $event['player'] . "\n";
+			}
+			else if ($event['event'] == "YELLOW_CARD") {
+				$marcatori .= $event['time'] ."'". "  🟨 - " . $event['player'] . "\n";
+			}
+			else if ($event['event'] == "RED_CARD" || $event['event'] == "YELLOW_RED_CARD") {
+				$marcatori .= $event['time'] ."'". "  🟥 - " . $event['player'] . "\n";
+			}
+			else if ($event['event'] == "MISSED_PENALTY") {
+				$marcatori .= $event['time'] ."'". " ®❌ - " . $event['player'] . "\n";
+			}
+		}
+	return $marcatori;
+}
+
+function sendMatch($chatId, $giornoPartita, $squadre, $risultato, $marcatori) {
 	global $client;
 	
 	$response = $client->sendMessage([
 		'chat_id' => $chatId,
 		'text' => $giornoPartita . "\n" . $squadre . "\n" . $risultato . "\n" . $marcatori
+	  ]);
+}
+
+function sendLeagues($chatId, $leauges) {
+	global $client;
+	
+	$response = $client->sendMessage([
+		'chat_id' => $chatId,
+		'text' => $leauges
 	  ]);
 }
 ?>
